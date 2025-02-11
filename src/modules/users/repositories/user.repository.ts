80ac4969e4018de13d4a88eb31d1ad7969a2f480
vitor <vitorgabriel181004@gmail.com>
2025-02-11 19:49:24 -0300

    fix: correção no import

--- src/modules/users/repositories/user.repository.ts
+++ src/modules/users/repositories/user.repository.ts
@@ -1,24 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/shared/prisma.service';
-import { CreateUserDto } from '../dtos/create-user.dto';
+import { User, Prisma } from '@prisma/client';
 
 @Injectable()
-export class UsersRepository {
+export class UserRepository {
   constructor(private readonly prisma: PrismaService) {}
 
-  async create(data: CreateUserDto) {
+  async createUser(data: Prisma.UserCreateInput): Promise<User> {
     return this.prisma.user.create({ data });
   }
 
-  async findByEmail(email: string) {
-    return this.prisma.user.findUnique({ where: { email } });
-  }
-
-  async findById(id: number) {
+  async findById(id: string): Promise<User | null> {
     return this.prisma.user.findUnique({ where: { id } });
   }
 
-  async update(id: number, data: Partial<CreateUserDto>) {
-    return this.prisma.user.update({ where: { id }, data });
+  async findByEmail(email: string): Promise<User | null> {
+    return this.prisma.user.findUnique({ where: { email } });
+  }
+
+  async findByCpf(cpf: string): Promise<User | null> {
+    return this.prisma.user.findUnique({ where: { cpf } });
   }
 }
