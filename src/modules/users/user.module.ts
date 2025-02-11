5b007030c6eb351192b4728b6f4c1b687769982d
vitor <vitorgabriel181004@gmail.com>
2025-02-11 19:49:13 -0300

    fix: correção no import

--- src/modules/users/user.module.ts
+++ src/modules/users/user.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './controllers/users.controller';
 import { UsersService } from './services/users.service';
-import { UsersRepository } from './repositories/user.repository';
+import { UserRepository } from './repositories/user.repository';
 import { PrismaService } from 'src/shared/prisma.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, UsersRepository, PrismaService],
+  providers: [UsersService, UserRepository, PrismaService],
   exports: [UsersService],
 })
 export class UsersModule {}
